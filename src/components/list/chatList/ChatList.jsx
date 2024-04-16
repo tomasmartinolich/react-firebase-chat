769b1138c9341438dba7843e0d8ce509380dd27a
Tomás Martinolich <tomasmartinolich@gmail.com>
2024-04-16 19:09:20 -0300

    terminando

--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -4,11 +4,14 @@ import AddUser from "./addUser/addUser"
 import { useUserStore } from "../../../lib/userStore";
 import { doc, getDoc, onSnapshot, updateDoc } from "firebase/firestore";
 import { db } from "../../../lib/firebase";
+import { useChatStore } from "../../../lib/chatStore";
 
 export default function ChatList() {
     const [addMode, setAddMode] = useState(false)
+    const [input, setInput] = useState("")
     const [chats, setChats] = useState([])
     const {currentUser} = useUserStore();
+    const {changeChat} = useChatStore();
 
     useEffect(() => {
         const unsub = onSnapshot(doc(db, "userchats", currentUser.id), async(res) => {
@@ -40,12 +43,38 @@ export default function ChatList() {
         })
     }, [])
 
+    const handleSelect = async (chat)=>{
+        const userChats = chats.map(item=>{
+            const {user, ...rest} = item;
+            return rest;
+        })
+
+        const chatIndex = userChats.findIndex(item=>item.chatId === chat.chatId)
+
+        userChats[chatIndex].isSeen = true;
+
+        const userChatsRef = doc(db,"userchats", currentUser.id);
+
+        try {      
+            await updateDoc(userChatsRef,{
+                chats: userChats
+            })
+            changeChat(chat.chatId, chat.user);
+        } catch (err) {
+            console.log(err)
+        }
+
+    }
+
+    const filteredChats = chats.filter(c=> 
+        c.user.username.toLowerCase().includes(input.toLowerCase()))
+
     return(
         <div className='chatList'>
             <div className="search">
                 <div className="searchBar">
                     <img src="/search.png" alt="" />
-                    <input type="text" placeholder="Search" />
+                    <input type="text" placeholder="Search" onChange={(e)=>setInput(e.target.value)} />
                 </div>
                 <img 
                 src={addMode ? "./minus.png" : "./plus.png"} 
@@ -53,16 +82,29 @@ export default function ChatList() {
                 className="add" 
                 onClick={()=> setAddMode((prev) => !prev)} />
             </div>
-            {chats.map((chat)=>{
-                <div className="item" key={chat.chatId}>
-                    <img src={chat.user.avatar || "./avatar.png"} alt="" />
+            {filteredChats.map((chat)=>{
+                <div 
+                    className="item" 
+                    key={chat.chatId} 
+                    onClick={()=>handleSelect(chat)}
+                    style={{
+                        backgroundColor: chat?.isSeen ? "transparent" : "#5183fe"
+                    }}
+                >
+                    <img src={chat.user.blocked.includes(currentUser.id) 
+                        ? "./avatar.png" 
+                        : chat.user.avatar || "./avatar.png"} alt="" />
                     <div className="texts">
-                        <span>{chat.user.username}</span>
+                        <span>{chat.user.blocked.includes(currentUser.id) 
+                        ? "User" 
+                        : chat.user.username}</span>
                         <p>{chat.lastMessage}</p>
                     </div>
                 </div>
             })}
+
             {addMode && <AddUser />}
+
         </div>
     )
 }
